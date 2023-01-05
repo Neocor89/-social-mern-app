cc5ba079dee1d85bb153d7a825bfec863a3408d1
Ben <ncor89b@gmail.com>
2023-01-05 15:59:14 +0100

    add dark mode logic and change favicon

--- client/src/components/FlexBetween.jsx
+++ client/src/components/FlexBetween.jsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import { styled } from "@mui/system";
 
+//: REUSABLE component Style
 const FlexBetween = styled(Box) ({
   display: 'flex',
   justifyContent: "space-between",
